42e5d0884de78e98237ffd4ee30c2bf93c733fc0
Tamas Balog <picimako@protonmail.com>
2022-10-05 08:42:29 +0200

    Upgrade gradle-intellij-plugin to 1.9.0 to prevent NoClassDefFoundException during test execution.

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,9 +10,8 @@ plugins {
     // GraphQL
     id("com.apollographql.apollo") version "2.5.13"
     // gradle-intellij-plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
-
-    // DO NOT UPGRADE UNTIL THE SNIPPET SEARCH LAUNCHES!!!
-    id("org.jetbrains.intellij") version "1.6.0"
+    // UPDATE TO v1.9.1 WHEN IT IS RELEASED TO FIX https://youtrack.jetbrains.com/issue/IDEA-298989/Duplicate-method-name-getFont.
+    id("org.jetbrains.intellij") version "1.9.0"
     // gradle-changelog-plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
     id("org.jetbrains.changelog") version "1.3.1"
 }
