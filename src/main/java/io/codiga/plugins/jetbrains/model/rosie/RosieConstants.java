406271c067eb9882ca88892ae16debdccbc05eab
Tamas Balog <picimako@protonmail.com>
2022-10-14 09:05:51 +0200

    Mark RosieConstants fields as final

--- src/main/java/io/codiga/plugins/jetbrains/model/rosie/RosieConstants.java
+++ src/main/java/io/codiga/plugins/jetbrains/model/rosie/RosieConstants.java
@@ -2,14 +2,14 @@ package io.codiga.plugins.jetbrains.model.rosie;
 
 public final class RosieConstants {
 
-    public static String ROSIE_FIX_ADD = "add";
-    public static String ROSIE_FIX_UPDATE = "update";
-    public static String ROSIE_FIX_REMOVE = "remove";
+    public static final String ROSIE_FIX_ADD = "add";
+    public static final String ROSIE_FIX_UPDATE = "update";
+    public static final String ROSIE_FIX_REMOVE = "remove";
 
-    public static String ROSIE_SEVERITY_CRITICAL = "critical";
-    public static String ROSIE_SEVERITY_ERROR = "error";
-    public static String ROSIE_SEVERITY_WARNING = "warning";
-    public static String ROSIE_SEVERITY_INFORMATIONAL = "informational";
+    public static final String ROSIE_SEVERITY_CRITICAL = "critical";
+    public static final String ROSIE_SEVERITY_ERROR = "error";
+    public static final String ROSIE_SEVERITY_WARNING = "warning";
+    public static final String ROSIE_SEVERITY_INFORMATIONAL = "informational";
 
     private RosieConstants() {
         // nothing
