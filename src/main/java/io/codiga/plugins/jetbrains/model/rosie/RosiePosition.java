f5bceba5352cecaa45f0ced06f3737bb57e743b5
Tamas Balog <picimako@protonmail.com>
2022-10-20 09:25:00 +0200

    Adjust column offset for RosieAnnotator

--- src/main/java/io/codiga/plugins/jetbrains/model/rosie/RosiePosition.java
+++ src/main/java/io/codiga/plugins/jetbrains/model/rosie/RosiePosition.java
@@ -29,9 +29,20 @@ public final class RosiePosition {
      */
     public int getOffset(@NotNull Editor editor) {
         Document document = editor.getDocument();
-        int offset = document.getLineStartOffset(this.line - 1) + this.col;
-        LOGGER.info(String.format("Converting line %s, col %s to offset: %s", this.line, this.col, offset));
+        int offset = document.getLineStartOffset(this.line - 1) + adjustColumnOffset(this.col);
+        LOGGER.info(String.format("Converting line %s, col %s to offset: %s", this.line, adjustColumnOffset(this.col), offset));
         return offset;
+    }
+
+    /**
+     * Adjusts the column offset by -1 since the column index returned by Codiga is 1-based, while the IDE editor is 0-based.
+     * <p>
+     * It doesn't adjust the offset if it is 0, so at the beginning of a line.
+     *
+     * @param columnOffset the adjusted offset
+     */
+    private int adjustColumnOffset(int columnOffset) {
+        return columnOffset != 0 ? (columnOffset - 1) : columnOffset;
     }
 
     @Override
