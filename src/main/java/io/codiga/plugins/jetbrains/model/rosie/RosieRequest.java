e2f0d007d14e65a19a9fd08599435e58f01a36aa
Tamas Balog <picimako@protonmail.com>
2022-10-26 09:27:27 +0200

    Simplify a few Rosie model classes with Lombok

--- src/main/java/io/codiga/plugins/jetbrains/model/rosie/RosieRequest.java
+++ src/main/java/io/codiga/plugins/jetbrains/model/rosie/RosieRequest.java
@@ -1,10 +1,13 @@
 package io.codiga.plugins.jetbrains.model.rosie;
 
+import lombok.AllArgsConstructor;
+
 import java.util.List;
 
 /**
  * Rosie request object sent to the Codiga API.
  */
+@AllArgsConstructor
 public class RosieRequest {
     public String filename;
     /**
@@ -21,14 +24,4 @@ public class RosieRequest {
     public String codeBase64;
     public List<RosieRule> rules;
     public boolean logOutput;
-
-
-    public RosieRequest(String filename, String language, String fileEncoding, String codeBase64, List<RosieRule> rosieRules, boolean logOutput) {
-        this.filename = filename;
-        this.language = language;
-        this.fileEncoding = fileEncoding;
-        this.codeBase64 = codeBase64;
-        this.rules = rosieRules;
-        this.logOutput = logOutput;
-    }
 }
--- src/main/java/io/codiga/plugins/jetbrains/model/rosie/RosieRuleResponse.java
+++ src/main/java/io/codiga/plugins/jetbrains/model/rosie/RosieRuleResponse.java
@@ -1,38 +1,18 @@
 package io.codiga.plugins.jetbrains.model.rosie;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 import java.util.List;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class RosieRuleResponse {
     public String identifier;
     public List<RosieViolation> violations;
     public List<String> errors;
     public String executionError;
     public String output;
-
-    public RosieRuleResponse() {
-        this.identifier = null;
-        this.violations = null;
-        this.errors = null;
-        this.executionError = null;
-        this.output = null;
-    }
-
-    public RosieRuleResponse(String identifier, List<RosieViolation> violations, List<String> errors, String executionError, String output) {
-        this.identifier = identifier;
-        this.violations = violations;
-        this.errors = errors;
-        this.executionError = executionError;
-        this.output = output;
-    }
-
-    @Override
-    public String toString() {
-        return "RuleResponse{" +
-            "identifier='" + identifier + '\'' +
-            ", violations=" + violations +
-            ", errors=" + errors +
-            ", executionError='" + executionError + '\'' +
-            ", output='" + output + '\'' +
-            '}';
-    }
 }
