9736c389e1aa64d26912acbcbe00413c1e3d1c30
Tamas Balog <picimako@protonmail.com>
2022-10-27 07:39:43 +0200

    Removed unused logger

--- src/main/java/io/codiga/plugins/jetbrains/settings/application/AppSettingsConfigurable.java
+++ src/main/java/io/codiga/plugins/jetbrains/settings/application/AppSettingsConfigurable.java
@@ -1,7 +1,6 @@
 package io.codiga.plugins.jetbrains.settings.application;
 
 import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.options.Configurable;
 import io.codiga.plugins.jetbrains.topics.ApiKeyChangeNotifier;
 import io.codiga.plugins.jetbrains.topics.CodigaEnabledStatusNotifier;
@@ -11,13 +10,11 @@ import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 
-import static io.codiga.plugins.jetbrains.Constants.LOGGER_NAME;
 import static io.codiga.plugins.jetbrains.topics.ApiKeyChangeNotifier.CODIGA_API_KEY_CHANGE_TOPIC;
 import static io.codiga.plugins.jetbrains.topics.CodigaEnabledStatusNotifier.CODIGA_ENABLED_CHANGE_TOPIC;
 import static io.codiga.plugins.jetbrains.topics.VisibilityKeyChangeNotifier.CODIGA_VISIBILITY_CHANGE_TOPIC;
 
 public class AppSettingsConfigurable implements Configurable {
-    private static final Logger LOGGER = Logger.getInstance(LOGGER_NAME);
     final ApiKeyChangeNotifier apiKeyChangeNotifier =
         ApplicationManager.getApplication().getMessageBus().syncPublisher(CODIGA_API_KEY_CHANGE_TOPIC);
     final VisibilityKeyChangeNotifier visibilityChangeNotifier =
