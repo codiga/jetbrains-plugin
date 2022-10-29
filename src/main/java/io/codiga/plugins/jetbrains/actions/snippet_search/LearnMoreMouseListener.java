d69c3c239f207ae9da14c8f9f2c9df1aca2f203b
Tamas Balog <picimako@protonmail.com>
2022-10-29 09:11:25 +0200

    Use MouseAdapter instead of MouseListener to simplify implementations

--- src/main/java/io/codiga/plugins/jetbrains/actions/snippet_search/LearnMoreMouseListener.java
+++ src/main/java/io/codiga/plugins/jetbrains/actions/snippet_search/LearnMoreMouseListener.java
@@ -3,8 +3,8 @@ package io.codiga.plugins.jetbrains.actions.snippet_search;
 import io.codiga.api.GetRecipesForClientSemanticQuery;
 
 import java.awt.*;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -12,7 +12,7 @@ import java.net.URISyntaxException;
 /**
  * Mouse listener to redirect the user to the login page.
  */
-public class LearnMoreMouseListener implements MouseListener {
+public class LearnMoreMouseListener extends MouseAdapter {
     private final GetRecipesForClientSemanticQuery.AssistantRecipesSemanticSearch snippet;
 
     public LearnMoreMouseListener(GetRecipesForClientSemanticQuery.AssistantRecipesSemanticSearch _snippet) {
@@ -30,25 +30,5 @@ public class LearnMoreMouseListener implements MouseListener {
         } catch (IOException | URISyntaxException e1) {
             e1.printStackTrace();
         }
-    }
-
-    @Override
-    public void mousePressed(MouseEvent e) {
-        // empty, nothing needed here
-    }
-
-    @Override
-    public void mouseReleased(MouseEvent e) {
-        // empty, nothing needed here
-    }
-
-    @Override
-    public void mouseEntered(MouseEvent e) {
-        // empty, nothing needed here
-    }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
-        // empty, nothing needed here
     }
 }
--- src/main/java/io/codiga/plugins/jetbrains/actions/snippet_search/OwnerMouseListener.java
+++ src/main/java/io/codiga/plugins/jetbrains/actions/snippet_search/OwnerMouseListener.java
@@ -1,8 +1,8 @@
 package io.codiga.plugins.jetbrains.actions.snippet_search;
 
 import java.awt.*;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -10,12 +10,13 @@ import java.net.URISyntaxException;
 /**
  * Mouse listener to redirect the user to the login page.
  */
-public class OwnerMouseListener implements MouseListener {
+public class OwnerMouseListener extends MouseAdapter {
     private final String slug;
 
     public OwnerMouseListener(String _slug) {
         this.slug = _slug;
     }
+
     @Override
     public void mouseClicked(MouseEvent e) {
         final String userLink = String.format("https://app.codiga.io/hub/user/%s", slug);
@@ -24,25 +25,5 @@ public class OwnerMouseListener implements MouseListener {
         } catch (IOException | URISyntaxException e1) {
             e1.printStackTrace();
         }
-    }
-
-    @Override
-    public void mousePressed(MouseEvent e) {
-        // empty, nothing needed here
-    }
-
-    @Override
-    public void mouseReleased(MouseEvent e) {
-        // empty, nothing needed here
-    }
-
-    @Override
-    public void mouseEntered(MouseEvent e) {
-        // empty, nothing needed here
-    }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
-        // empty, nothing needed here
     }
 }
